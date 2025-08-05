4c71f671b33ec91939b4c348fa94c7494a85fdb6
jorgefast79 <89618628+jorgefast79@users.noreply.github.com>
2025-08-04 20:48:27 -0600

    Primer commit: proyecto Spring Boot con OTP

---
+++ src/main/java/ufgfans/com/ufgfans/UfgfansApplication.java
@@ -0,0 +1,13 @@
+package ufgfans.com.ufgfans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfgfansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfgfansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufgfans/com/ufgfans/UfgfansApplicationTests.java
@@ -0,0 +1,13 @@
+package ufgfans.com.ufgfans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfgfansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
